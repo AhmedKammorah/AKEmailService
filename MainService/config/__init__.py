7dad8d6587367d94dc4f9f889a9bd63a44db91bf
Ahmed kammorah <Ahmedkammorah@gmail.com>
2019-04-05 15:23:55 +0200

    Add the main project structure for the service

---
+++ MainService/config/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: Ahmed kammorah
+# @Date:   2019-04-05 15:22:53
+# @Last Modified by:   Ahmed kammorah
+# @Last Modified time: 2019-04-05 15:22:53
---
+++ MainService/main/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: ahmedkammorah
+# @Date:   2019-04-04 11:42:55
+# @Last Modified by:   ahmedkammorah
+# @Last Modified time: 2019-04-04 11:42:55
