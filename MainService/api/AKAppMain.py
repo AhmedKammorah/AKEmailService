dba83c36dd881ab24a565345b8d4f51ddbddd2fa
Ahmed kammorah <Ahmedkammorah@gmail.com>
2019-04-08 02:28:48 +0200

    fix and document the send email endpoint

--- MainService/api/AKAppMain.py
+++ MainService/api/AKAppMain.py
@@ -2,27 +2,90 @@
 # @Author: ahmedkammorah
 # @Date:   2017-01-21 16:58:27
 # @Last Modified by:   Ahmed kammorah
-# @Last Modified time: 2019-04-06 20:50:28
+# @Last Modified time: 2019-04-08 02:14:23
 from pprint import pprint
-from ak_base_api import *
+from MainService.api.ak_base_api import *
 from MainService.main.ak_main_email_service import AKMainEmailService, EmailMessage
 
 email_service = AKMainEmailService()
 
 @app.route('/api/email', methods=['POST'])
-# @jwt_required()
-# @swag_from('./swagger/swagger.yml')
+@jwt_required()
 def send_email():
     """
-    file: swagger/email.yaml
+    ---
+    tags:
+        - "AKEmailServiceAPI"
+    summary: "Send email API Endpoint"
+    description: "Send email API Endpoint"
+    operationId: "sendEmail"
+    consumes:
+      - "application/json"
+      - "application/xml"
+    produces:
+      - "application/xml"
+      - "application/json"
+    parameters:
+      - in: "body"
+        name: "body"
+        description: "Full email message"
+        required: true
+        schema:
+          $ref: "#/definitions/EmailMessage"
+    responses:
+      405:
+        description: "Invalid input"
+    definitions:
+      EmailMessage:
+        type: "object"
+        required:
+        - "from_email"
+        - "to_emails"
+        properties:
+          subject:
+            type: "string"
+            example: "hello"
+          from_email:
+            type: "string"
+            example: "ahmed@trendy.com"
+          to_emails:
+            type: "array"
+            xml:
+              name: "to_emails"
+              wrapped: true
+            items:
+              type: "string"
+          body:
+            type: "string"
+            description: "full email body"
+        xml:
+          name: "EmailMessage"
+        example:
+          to_emails:
+          - "ahmedkammorah@gmail.com"
+          - "ahmedkammorah+1@gmail.com"
+          from_email: "ahmed@gmail.com"
+          subject: "hello"
+          body: "body"
+      ApiResponse:
+        type: "object"
+        properties:
+          code:
+            type: "integer"
+            format: "int32"
+          type:
+            type: "string"
+          message:
+            type: "string"
+
     """
-    
     email_message = request.get_json()
     pprint(email_message)
 
     msg = EmailMessage(to_emails=email_message['to_emails'], from_email=email_message['from_email'], subject=email_message['subject'], body=email_message['body'])
     if msg == None:
         return app.make_response(("one of the required feilds is not fullfiled", 400))    
+    
     email_service.send_email(msg)
     app.logger.debug("Sending email by email service")
     return app.make_response(("Successfully send email", 200))
---
+++ MainService/api/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: Ahmed kammorah
+# @Date:   2019-04-07 15:45:47
+# @Last Modified by:   Ahmed kammorah
+# @Last Modified time: 2019-04-07 15:45:48
