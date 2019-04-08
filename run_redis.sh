82113110afd01400bbf400cde512cfecbeaf18f8
Ahmed kammorah <Ahmedkammorah@gmail.com>
2019-04-08 02:32:48 +0200

    script to run redis for development phase

--- run_redis.sh
+++ run_redis.sh
@@ -1,5 +1,7 @@
 # @Author: Ahmed kammorah
 # @Date:   2019-04-06 01:13:33
 # @Last Modified by:   Ahmed kammorah
-# @Last Modified time: 2019-04-06 01:13:46
+# @Last Modified time: 2019-04-08 00:17:52
+docker stop ak-redis
+docker rm -f ak-redis
 docker run --name ak-redis -d redis
